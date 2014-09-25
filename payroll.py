0df6e42383cb478ab1adde1e51b41ef6f46ef860
KrConrad <chocokrizx@gmail.com>
2014-09-25 10:54:48 -0600

    Create payroll.py
    This is a little payroll and overtime calculator I was playing with.

---
+++ payroll.py
@@ -0,0 +1,19 @@
+def calculatepay(h,r):
+    if h<=40:
+    p=r*h
+    else:
+    p=r*40+(r*1.5*(h-40))
+    return p
+  
+try: 
+    inp=raw_input("Enter Hours!!!11: ")
+    hours=float(inp)
+    inp=raw_input("Enter the rate amg!: ")
+    rate=float(inp)
+
+except:
+    print "Error, you didn't enter a numeric input =(")
+    quit()
+  
+pay=calculatepay(hours, rate)
+print pay
